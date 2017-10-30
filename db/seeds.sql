665524b3a45e7c34db65dc760068fd3840f27df7
Cants820 <Cantosjoseph@gmail.com>
2017-10-29 22:34:08 -0700

    none of this does not work

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name,devoured,burger_date) VALUES ("Cheese Burger", false , "2017-10-28 08:30:00" );
+INSERT INTO burgers (burger_name,devoured,burger_date) VALUES ("Shredded Spicy Pork", false , "2017-10-28 10:30:00" );
+INSERT INTO burgers (burger_name,devoured,burger_date) VALUES ("Cheese Burger", false , "2017-10-30 12:20:00" );
+
+
+
+
